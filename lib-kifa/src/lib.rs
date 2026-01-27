18e2643be1c20c91de8284730c35ab547fc199e6
Success Kingsley <successxodev@gmail.com>
2026-01-27 13:45:19 -0500

    feat: Implement sector-aligned memory buffers and write-ahead log

--- lib-kifa/src/lib.rs
+++ lib-kifa/src/lib.rs
@@ -1,15 +1,13 @@
-#[must_use]
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![warn(clippy::pedantic)]
+#![allow(
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    unused
+)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[macro_use]
+pub mod helpers;
+pub(crate) mod buffer;
+pub(crate) mod wal;
